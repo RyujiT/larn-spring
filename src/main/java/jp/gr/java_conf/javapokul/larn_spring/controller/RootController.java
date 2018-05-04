b0c58fa2389665a88f002ba98670f9efe0e260e5
rtaba <the.sun.also.rises.2nd@gmail.com>
2018-05-04 19:56:01 +0900

    ロガー追加

--- src/main/java/jp/gr/java_conf/javapokul/larn_spring/controller/RootController.java
+++ src/main/java/jp/gr/java_conf/javapokul/larn_spring/controller/RootController.java
@@ -1,13 +1,19 @@
 package jp.gr.java_conf.javapokul.larn_spring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
 
+	/** ロガー. */
+	private final Logger logger = LoggerFactory.getLogger(RootController.class);
+
 	@RequestMapping("/")
 	public String index() {
+		logger.debug("ルートが呼び出されたよ");
 		return "起動中だよ";
 	}
 }
